ceb36537d187476abebd9db468542251069b2b16
Frans Chokoe <fmoyagabo@gmail.com>
2026-01-19 00:52:07 +0200

    fixed not found

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ function NotFound(){
     return(
         <div>
             <h1>404 Not Found</h1>
-            <p>The page you are looking for does not exist 👀</p>
+            <p>The page you are looking for does not exist 😭</p>
         </div>
     )
 }
